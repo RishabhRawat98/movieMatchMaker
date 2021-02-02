abfee46a1b7edf2d9ae520538523d7fb1c7575d4
Tara <TaraHussain97@gmail.com>
2021-02-02 03:57:57 +0000

    start again

--- client/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Register } from './Register';
-export { default as Login } from './Login';
-export { default as MovieList } from './MovieList';
-export { default as Swipe } from './Swipe';
-export { default as Welcome } from './Welcome';
