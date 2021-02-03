df7c9452483b2202ce71f3014e86b8a041341321
Dragos Suciu <dragos.suciu.15@alumni.ucl.ac.uk>
2021-02-03 19:43:14 +0200

    movie list page edits

--- app/api/project/__init__.py
+++ app/api/project/__init__.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager 
-
+from flask_login import UserMixin
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
--- app/api/project/models.py
+++ app/api/project/models.py
@@ -49,60 +49,60 @@ class User(UserMixin, db.Model):
 # for m in movie:
 #     print(m.title, m.id, m.poster_path)
 ###########################THe good code 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy 
-import csv
-from io import TextIOWrapper
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy 
+# import csv
+# from io import TextIOWrapper
 
-app = Flask(__name__)
-app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///znewmovie.db'
-db = SQLAlchemy(app)
+# app = Flask(__name__)
+# app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///znewmovie.db'
+# db = SQLAlchemy(app)
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String)
+# # class User(db.Model):
+# #     id = db.Column(db.Integer, primary_key=True)
+# #     username = db.Column(db.String)
 
-#     def __repr__(self):
-#         return f"User('{self.username}')"
-
-
-#,overview,poster_path,release_date,title,genre1,genre2
-
-class Moviedata(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   number = db.Column(db.Integer)
-   overview = db.Column(db.Text)
-   poster_path = db.Column(db.Text)
-   release_date = db.Column(db.Text)
-   title = db.Column(db.Text)
-   genre1 = db.Column(db.Integer)
-   genre2 = db.Column(db.Integer)
-
-   def __repr__(self):
-       return f"Moviedata('{self.title}', '{self.overview}', '{self.poster_path}', '{self.release_date}', '{self.genre1}', '{self.genre2}')"
+# #     def __repr__(self):
+# #         return f"User('{self.username}')"
 
 
-db.create_all()
+# #,overview,poster_path,release_date,title,genre1,genre2
+
+# class Moviedata(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    number = db.Column(db.Integer)
+#    overview = db.Column(db.Text)
+#    poster_path = db.Column(db.Text)
+#    release_date = db.Column(db.Text)
+#    title = db.Column(db.Text)
+#    genre1 = db.Column(db.Integer)
+#    genre2 = db.Column(db.Integer)
+
+#    def __repr__(self):
+#        return f"Moviedata('{self.title}', '{self.overview}', '{self.poster_path}', '{self.release_date}', '{self.genre1}', '{self.genre2}')"
 
 
-file_name = "Use.csv"
-
-def runner():
-    with open(file_name, 'r', encoding='utf-8') as f:
-        new_reader = csv.reader(f)
-        # new_reader = TextIOWrapper(new_reader, encoding='utf-8')
-        # rows = csv.reader(new_reader, delimiter=',')
-        rows=list(new_reader)
-        print(rows[0])
-        for row in rows:
-            movie = Moviedata(number=row[0],overview=row[1], poster_path=row[2], release_date=row[3], title=row[4], genre1=row[5], genre2=row[6])
-            db.session.add(movie)
-            db.session.commit()
-            # print(row[0])
+# db.create_all()
 
 
-runner()
+# file_name = "Use.csv"
+
+# def runner():
+#     with open(file_name, 'r', encoding='utf-8') as f:
+#         new_reader = csv.reader(f)
+#         # new_reader = TextIOWrapper(new_reader, encoding='utf-8')
+#         # rows = csv.reader(new_reader, delimiter=',')
+#         rows=list(new_reader)
+#         print(rows[0])
+#         for row in rows:
+#             movie = Moviedata(number=row[0],overview=row[1], poster_path=row[2], release_date=row[3], title=row[4], genre1=row[5], genre2=row[6])
+#             db.session.add(movie)
+#             db.session.commit()
+#             # print(row[0])
 
 
-#This is what the columns for the sql are 
-# number,overview,poster_path,release_date,title,genre1,genre2 
+# runner()
+
+
+# #This is what the columns for the sql are 
+# # number,overview,poster_path,release_date,title,genre1,genre2 
