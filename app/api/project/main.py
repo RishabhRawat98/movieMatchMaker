161095f5ab4c449668fe591ed8a566bd39a297f2
Dragos Suciu <dragos.suciu.15@alumni.ucl.ac.uk>
2021-02-03 18:06:47 +0200

    movie list page

--- app/api/project/main.py
+++ app/api/project/main.py
@@ -9,7 +9,12 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
-@main.route('/profile')
+@main.route('/swipe')
 @login_required
-def profile():
-    return render_template('profile.html', name=current_user.name, movieId="97889" , movieImageURL="https://lumiere-a.akamaihd.net/v1/images/ct_aladdin2019_aladdin_17975_53d203c9.jpeg" , movieTitle="Aladdin", movieBrief="Aladdin, a kind thief, woos Jasmine, the princess of Agrabah, with the help of Genie. When Jafar, the grand vizier, tries to usurp the king, Jasmine, Aladdin and Genie must stop him from succeeding.")
+def swipe():
+    return render_template('swipe.html', name=current_user.name, movieId="97889" , movieImageURL="https://lumiere-a.akamaihd.net/v1/images/ct_aladdin2019_aladdin_17975_53d203c9.jpeg" , movieTitle="Aladdin", movieBrief="Aladdin, a kind thief, woos Jasmine, the princess of Agrabah, with the help of Genie. When Jafar, the grand vizier, tries to usurp the king, Jasmine, Aladdin and Genie must stop him from succeeding.")
+
+@main.route('/movielist')
+@login_required
+def movielist():
+    return render_template('movielist.html')
--- app/api/project/models.py
+++ app/api/project/models.py
@@ -12,9 +12,9 @@ from . import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    name = db.Column(db.String(1000), nullable=False)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
 
 # def Load_movies(file_name):
 #     data = csv.reader(file_name, delimiter = ',')
